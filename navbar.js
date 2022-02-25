1dcefa193310e367f932bc3f680a3b89556cc112
malikkanika9 <97448286+malikkanika9@users.noreply.github.com>
2022-02-25 12:27:22 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,32 @@
+function navBar(){
+return `
+
+        <div id="pinkline">UP TO 40% OFF FOR A LIMITED TIME. SHOP THE SALE details</div>
+    <div id="blueline">FREE SHIPPING ON ALL ORDERS| details</div>
+    <div id="whitline">
+        <div id="left"><a id="ref" href="">Stores & Services</a>&nbsp;&nbsp;&nbsp;&nbsp;<a id="ref" href="">Rewards</a>&nbsp;&nbsp;&nbsp;&nbsp;<a id="ref"href="">ChatLive</a></div>
+        <div id="medium"><a id="ref" href="home.html"><img src="https://cdn.shopify.com/s/files/1/0637/6147/files/logo_556396e7-3386-4a9c-8dfc-b9250175595e.png?height=628&pad_color=ffffff&v=1523377568&width=1200" width="130px" height="70px"alt=""></a></div>
+        <div id="right"><img src="https://www.freeiconspng.com/thumbs/search-icon-png/search-icon-png-1.png" width="50px" height="50px" alt=""></div>
+   <input type="text" placeholder="search" id="searchbar" ><img id="aclogo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThRGcLNaorK4esT7jd4P_MfhhrzowqyTHRqA8Ku2vZW7KNrswJYoA0CcmhlTTPsWSQZ5I&usqp=CAU" width="30px" height="30px"alt="">
+    <img id="aclogo" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2018/240/iconmonstr-heart-thin.png" width="30px" height="30px" alt=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgJpV_Q-MKeIRt9KzLZInQCzygSyKaxNo09NtSfLFajfxK0VQfVtRbWcsWls8I58-Zbo4&usqp=CAU" id="aclogo" width="30px" height="30px" alt=""></div><hr>
+    <div id="menu"><ul id="unorder">
+        <li id="mitem"><a href="" id="ref">BestSellers</a></li>
+        <li id="mitem"><a href="" id="ref">New</a></li>
+        <li id="mitem"><a href="" id="ref">Brands</a></li>
+        <li id="mitem"><a href="skincare.html" id="ref">Skincare</a></li>
+        <li id="mitem"><a href="makeup.html" id="ref">Makeup</a></li>
+        <li id="mitem"><a href="makeup.html" id="ref">Hair</a></li>
+        <li id="mitem"><a href="" id="ref">Bath & Body</a></li>
+        <li id="mitem"> <a href="" id="ref">Fragrance</a></li>
+        <li id="mitem"><a href="" id="ref">Gifts</a></li>
+        <li id="mitem"><a href="" id="ref">Sales</a></li>
+        <li id="mitem"><a href="" id="ref">Featured</a></li>
+    </ul></div> <hr id="bottom">
+    <div id="secondwhite"> FREE SHIPPING ON ALL ORDERS!</div>
+   
+
+`
+
+}
+
+export default navBar;
